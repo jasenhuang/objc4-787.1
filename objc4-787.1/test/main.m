7893b26d68cf828482e3a2f94b76ec6d4e8454a6
jasenhuang <jasenhuang@tencent.com>
2020-12-23 17:56:39 +0800

    init

---
+++ objc4-787.1/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by 黄栩生 on 2020/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
